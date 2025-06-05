b5d1d21099831ff0bd5fd6f314e3997551c4e30f
ghjiukliop <152683933+ghjiukliop@users.noreply.github.com>
2025-06-05 23:11:34 +0700

    Update garden.lua

--- garden.lua
+++ garden.lua
@@ -475,7 +475,7 @@ local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
 assert(PlayTab, "[AutoPlant] PlayTab chưa được tạo!")
-local PlantSection = PlayTab:AddSection("🌱 Auto Plant Seed")
+local PlantSection = PlayTab:AddSection("🌱2 Auto Plant Seed")
 
 local selectedSeedsToPlant = ConfigSystem.CurrentConfig.SelectedSeeds or {}
 local autoPlantEnabled = ConfigSystem.CurrentConfig.AutoPlantEnabled or false
